730653bf70d63ae07488ad248045595a4cac9db8
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-18 10:41:07 -0400

    vid 33 react Route setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+//lets us call Router and it will be BrowserRouter
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App" >
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
