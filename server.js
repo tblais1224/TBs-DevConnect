c77c0c22d3714a28158124a74f570012162277e5
tblais1224 <thomasblais.student@careerdevs.com>
2019-06-05 15:55:20 -0400

    add script to json and add prod run to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
 const passport = require('passport')
+const path = require('path')
 
 const users = require("./routes/api/users")
 const profile = require("./routes/api/profile")
@@ -32,6 +33,17 @@ require("./config/passport")(passport);
 app.use("/api/users", users)
 app.use("/api/profile", profile)
 app.use("/api/posts", posts)
+
+//the below code is needed to run in production with react build
+//Server static assets if in production
+if(process.env.NODE_ENV === "production"){
+  // set static folder
+  app.use(express.static("client/build"))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 const port = process.env.PORT || 5000;
 
